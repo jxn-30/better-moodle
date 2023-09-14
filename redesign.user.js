50d06ece834cc0d5afa4b582752062abfb771a82
jan <jan.boehmerle@ledbrain.de>
2023-09-14 18:13:46 +0200

    feat: release 1.2.3

--- redesign.user.js
+++ redesign.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            🎓️ UzL: Better Moodle
 // @namespace       https://uni-luebeck.de
-// @version         1.2.2
+// @version         1.2.3
 // @author          Jan (jxn_30)
 // @description:de  Verbessert dieses seltsame Design, das Moodle 4 mit sich bringt
 // @homepage        https://github.com/jxn-30/better-moodle
