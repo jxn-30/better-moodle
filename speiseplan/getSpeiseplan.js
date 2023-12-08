e9cf4c5debebb9d9f272eeaa74ff457b889aebdb
jan <jan.boehmerle@ledbrain.de>
2023-12-08 22:41:51 +0100

    refactor(speiseplan): get english speiseplan

--- speiseplan/getSpeiseplan.js
+++ speiseplan/getSpeiseplan.js
@@ -9,6 +9,40 @@ const filterTypes = {
 Object.seal(filterTypes);
 Object.freeze(filterTypes);
 
+/**
+ * @typedef {Object} Filter
+ * @property {string} title
+ * @property {string} abk
+ * @property {string} [img]
+ */
+
+/**
+ * @typedef {Object} Speise
+ * @property {boolean} mensa
+ * @property {{name: string, zusatz?: string}[]} items
+ * @property {string[]} allergene
+ * @property {string[]} zusatzstoffe
+ * @property {string[]} arten
+ * @property {number[]} preise
+ */
+
+/**
+ * @typedef {Object} Speiseplan
+ * @property {Record<string, Speise[]>} speisen
+ */
+
+/**
+ * @typedef {Object} SpeiseplanJSON
+ * @property {number} lastUpdate
+ * @property {Speiseplan} de
+ * @property {Speiseplan} en
+ */
+
+/**
+ * @param {HTMLDocument} document
+ * @param {Node} Node
+ * @returns {Speiseplan.speisen}
+ */
 const getSpeisen = (document, Node) => {
     const speisen = {};
     document.querySelectorAll('.mensa_menu_detail').forEach(speise => {
@@ -17,7 +51,7 @@ const getSpeisen = (document, Node) => {
         const items = [{ name: '' }];
         speise.querySelector('.menu_name').childNodes.forEach(node => {
             if (node.nodeType === Node.TEXT_NODE) {
-                items.at(-1).name += ` ${node.textContent.trim()}`;
+                items.at(-1).name += node.textContent.trim();
             }
             if (node.nodeType === Node.ELEMENT_NODE) {
                 if (node.tagName === 'BR') {
@@ -43,47 +77,74 @@ const getSpeisen = (document, Node) => {
                 .map(p => parseFloat(p)),
         });
     });
+    Object.values(speisen).forEach(day =>
+        day.sort(
+            (a, b) =>
+                b.mensa - a.mensa || // mensa first
+                JSON.stringify(a.items).localeCompare(JSON.stringify(b.items)) // then sort by items
+        )
+    );
     return speisen;
 };
 
-fetch('https://studentenwerk.sh/de/mensen-in-luebeck?ort=3&mensa=8')
-    .then(res => res.text())
-    .then(html => new JSDOM(html))
-    .then(({ window: { document, Node, URL } }) => {
-        const filters = {};
-        document.querySelectorAll('.filterbutton').forEach(filter => {
-            const type = filterTypes[filter.dataset.typ];
-            filters[type] ??= {};
-            const img = filter.querySelector('img')?.src ?? '';
-            filters[type][filter.dataset.wert] = {
-                title:
-                    filter
-                        .querySelector('span:not(.abk)')
-                        ?.textContent?.trim() ?? '',
-                abk:
-                    filter.querySelector('span.abk')?.textContent?.trim() ?? '',
-                img: img && new URL(img, 'https://studentenwerk.sh/').href,
-            };
-        });
-        Object.freeze(filters);
-        return { filters, speisen: getSpeisen(document, Node) };
-    })
-    .then(speiseplan =>
-        fetch(
-            'https://studentenwerk.sh/de/mensen-in-luebeck?ort=3&mensa=8&nw=1'
-        )
-            .then(res => res.text())
-            .then(html => new JSDOM(html))
-            .then(({ window: { document, Node } }) => {
-                speiseplan.speisen = {
-                    ...speiseplan.speisen,
-                    ...getSpeisen(document, Node),
-                };
-                return speiseplan;
-            })
+const localizedPath = {
+    de: 'mensen-in-luebeck',
+    en: 'food-overview',
+};
+Object.seal(localizedPath);
+Object.freeze(localizedPath);
+
+/**
+ * @param {string} lang
+ * @returns {Promise<{speisen: {}, filters: {}}>}
+ */
+const getLocalizedSpeiseplan = lang =>
+    fetch(
+        `https://studentenwerk.sh/${lang}/${localizedPath[lang]}?ort=3&mensa=8`
     )
-    .then(speiseplan =>
-        fs.writeFile('speiseplan.json', JSON.stringify(speiseplan, null, 4), {
-            encoding: 'utf-8',
+        .then(res => res.text())
+        .then(html => new JSDOM(html))
+        .then(({ window: { document, Node, URL } }) => {
+            const filters = {};
+            document.querySelectorAll('.filterbutton').forEach(filter => {
+                const type = filterTypes[filter.dataset.typ];
+                filters[type] ??= {};
+                const img = filter.querySelector('img')?.src ?? undefined;
+                filters[type][filter.dataset.wert] = {
+                    title:
+                        filter
+                            .querySelector('span:not(.abk)')
+                            ?.textContent?.trim() ?? '',
+                    abk:
+                        filter.querySelector('span.abk')?.textContent?.trim() ??
+                        '',
+                    img: img && new URL(img, 'https://studentenwerk.sh/').href,
+                };
+            });
+            Object.freeze(filters);
+            return { filters, speisen: getSpeisen(document, Node) };
         })
-    );
+        .then(speiseplan =>
+            fetch(
+                `https://studentenwerk.sh/${lang}/${localizedPath[lang]}?ort=3&mensa=8&nw=1`
+            )
+                .then(res => res.text())
+                .then(html => new JSDOM(html))
+                .then(({ window: { document, Node } }) => {
+                    speiseplan.speisen = {
+                        ...speiseplan.speisen,
+                        ...getSpeisen(document, Node),
+                    };
+                    return speiseplan;
+                })
+        );
+Promise.all([getLocalizedSpeiseplan('de'), getLocalizedSpeiseplan('en')]).then(
+    ([de, en]) =>
+        fs.writeFile(
+            'speiseplan.json',
+            JSON.stringify({ lastUpdate: Date.now(), de, en }, null, 4),
+            {
+                encoding: 'utf-8',
+            }
+        )
+);
