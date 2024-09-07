2223c328c042efe32f190110c545195110f06066
jan <jan.boehmerle@ledbrain.de>
2024-09-07 21:42:47 +0200

    feat(externalLinks): implement feature

--- src/features/general/externalLinks.ts
+++ src/features/general/externalLinks.ts
@@ -1,10 +1,52 @@
 import { BooleanSetting } from '../../_lib/Settings/BooleanSetting';
 import Feature from '../../_lib/Feature';
-import Setting from '../../_lib/Setting';
 
-const settings = new Set<Setting>();
-settings.add(new BooleanSetting('enabled', true));
+const enabled = new BooleanSetting('enabled', true).onInput(() => {
+    // TODO: this.feature.reload() once `this` set
+    enabled.feature?.reload();
+});
+
+const modifiedTargets = new Map<HTMLAnchorElement, string>();
+
+/**
+ * An event listener setting the target to '_blank' for clicked links of their targets origin does not match the current origin.
+ * @param event - the MouseEvent that is being fired
+ */
+const openInNewTabListener = (event: MouseEvent) => {
+    const target = event.target;
+    if (!(target instanceof HTMLAnchorElement) || target.target) return;
+    const { origin, protocol } = new URL(target.href, window.location.origin);
+    if (protocol === 'javascript:') return;
+    if (
+        origin &&
+        origin !== window.location.origin &&
+        target.target !== '_blank'
+    ) {
+        modifiedTargets.set(target, target.target);
+        target.target = '_blank';
+    }
+};
+
+/**
+ * Adds the event listener that handles mouse clicks
+ */
+const onload = () => {
+    if (enabled.value) document.addEventListener('click', openInNewTabListener);
+};
+
+/**
+ * Removes the event listener that handles mouse clicks
+ */
+const onunload = () => {
+    document.removeEventListener('click', openInNewTabListener);
+    modifiedTargets.forEach(
+        (origTarget, anchor) => (anchor.target = origTarget)
+    );
+    modifiedTargets.clear();
+};
 
 export default Feature.register({
-    settings,
+    settings: new Set([enabled]),
+    onload,
+    onunload,
 });
