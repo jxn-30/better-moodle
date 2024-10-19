f2dd5236fde04a0d5d0a49d049c923327383b558
jan <jan.boehmerle@ledbrain.de>
2024-10-19 20:25:55 +0200

    feat(types): add types for template contexts

--- types/require.js/core/templates.d.ts
+++ types/require.js/core/templates.d.ts
@@ -1,11 +1,21 @@
 // types found out by reading the moodle source code
 // may be incomplete at some points
 
+import CoreMoreMenuChildren from './_templates/core/moremenu_children';
+
+type Context = string | number | Context[] | Record<string, Context>;
+
 export default interface CoreTemplates {
-    renderForPromise(
-        templateName: string,
-        context?: unknown,
+    renderForPromise<Template extends keyof ContextMap>(
+        templateName: Template,
+        context: ContextMap[Template],
         themeName?: string
     ): Promise<{ html: string; js: string }>;
     renderPix(key: string, component: string, title?: string): Promise<string>;
+}
+
+interface ContextMap extends Record<string, Context> {
+    'core/loading': Record<string, never>;
+    'core/moremenu_children': CoreMoreMenuChildren;
+    'core_course/favouriteicon': { isfavourite: boolean; id: number };
 }
