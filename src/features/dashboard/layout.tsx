441d5ad0a93159b452c0b18e3c759b049baa6e44
jan <jan.boehmerle@ledbrain.de>
2024-11-26 13:08:25 +0100

    feat(dashboard): fully implement course filters

--- src/features/dashboard/layout.tsx
+++ src/features/dashboard/layout.tsx
@@ -3,12 +3,14 @@ import Feature from '@/Feature';
 import { getLoadingSpinner } from '@/DOM';
 import { LL } from '../../i18n/i18n';
 import { requirePromise } from '@/require.js';
-import Drawer, { Side } from '@/Drawer';
+import style from './layout.module.scss';
 import {
+    type CourseFilter,
     getActiveFilter,
     getAvailableCourseFilters,
     onActiveFilterChanged,
 } from '@/myCourses';
+import Drawer, { Side } from '@/Drawer';
 import { isDashboard, isLoggedIn } from '@/helpers';
 
 const coursesSidebarEnabled = new BooleanSetting(
@@ -22,13 +24,26 @@ const rightSidebarEnabled = new BooleanSetting(
     true
 ).requireReload();
 
-// const courseFilter = '_sync';
+// migrate the old storage of courseFilter to new one
+const courseFilterStorageKey = 'dashboard.layout.courseFilter';
+const oldCourseFilterStorageKey =
+    'better-moodle-settings.dashboard.courseListFilter';
+const oldActiveCourseFilter = JSON.parse(
+    GM_getValue(oldCourseFilterStorageKey, JSON.stringify('_sync'))
+) as CourseFilter | '_sync';
+GM_deleteValue(oldCourseFilterStorageKey);
+let courseFilter: CourseFilter | '_sync' = GM_getValue(
+    courseFilterStorageKey,
+    oldActiveCourseFilter
+);
+GM_setValue(courseFilterStorageKey, courseFilter);
 
 /**
  * Loads the content of the courses sidebar and shows a loading spinner meanwhile
  * @param drawer - the Drawer to add the course content to
+ * @param filterSelection
  */
-const loadCourseContent = (drawer: Drawer) => {
+const loadCourseContent = (drawer: Drawer, filterSelection: HTMLDivElement) => {
     let contentLoaded = false;
     // TODO: Do not create a new loadingSpinner but reuse the old one?
     void getLoadingSpinner().then(spinner => {
@@ -39,7 +54,9 @@ const loadCourseContent = (drawer: Drawer) => {
     });
 
     void Promise.all([
-        getAvailableCourseFilters().then(getActiveFilter),
+        courseFilter === '_sync' ?
+            getAvailableCourseFilters().then(getActiveFilter)
+        :   Promise.resolve(courseFilter),
         requirePromise(['block_myoverview/repository'] as const),
     ])
         .then(([filter, [myCourses]]) => {
@@ -48,6 +65,7 @@ const loadCourseContent = (drawer: Drawer) => {
                     "Couldn't find a filter to use for fetching courses."
                 );
             }
+
             return myCourses.getEnrolledCoursesByTimeline({
                 classification: filter.classification,
                 customfieldname: filter.customfieldname,
@@ -63,10 +81,13 @@ const loadCourseContent = (drawer: Drawer) => {
             if (!courses.length) {
                 return drawer.setContent(
                     (
-                        <span className="text-muted text-center">
-                            {LL.features.dashboard.features.layout.myCourses.empty()}
-                        </span>
-                    ) as HTMLSpanElement
+                        <>
+                            {filterSelection}
+                            <span className="text-muted text-center">
+                                {LL.features.dashboard.features.layout.myCourses.empty()}
+                            </span>
+                        </>
+                    ) as DocumentFragment
                 );
             }
 
@@ -99,8 +120,150 @@ const loadCourseContent = (drawer: Drawer) => {
             );
 
             return drawer.setContent(
-                (<>{...courseCards}</>) as DocumentFragment
+                (
+                    <>
+                        {filterSelection}
+                        {...courseCards}
+                    </>
+                ) as DocumentFragment
             );
+        });
+};
+
+/**
+ *
+ */
+const initCourseSidebar = () => {
+    const courseFilterDropdownBtnId = `${style.coursesSidebarFilterMenu}-toggle`;
+    const filterSelection = (
+        <div
+            id={style.coursesSidebarFilterMenu}
+            class="list-group w-100 mb-6 hidden"
+            aria-labelledby={courseFilterDropdownBtnId}
+        ></div>
+    ) as HTMLDivElement;
+
+    const syncFilterButton = (
+        <button type="button" class="list-group-item list-group-item-action">
+            [{LL.features.dashboard.features.layout.myCourses.sync()}]
+        </button>
+    ) as HTMLButtonElement;
+
+    const filterButtons = new Map<HTMLButtonElement, CourseFilter | '_sync'>();
+    filterButtons.set(syncFilterButton, '_sync');
+
+    void getAvailableCourseFilters()
+        .then(filters =>
+            filterSelection.replaceChildren(
+                syncFilterButton,
+                ...filters.map(filter => {
+                    const btn = (
+                        <button
+                            type="button"
+                            class="list-group-item list-group-item-action"
+                        >
+                            {filter.name}
+                        </button>
+                    ) as HTMLButtonElement;
+                    filterButtons.set(btn, filter);
+                    return btn;
+                })
+            )
+        )
+
+        .then(getActiveFilter)
+        .then(activeFilter => {
+            if (courseFilter === '_sync') {
+                syncFilterButton.classList.add('active');
+                return;
+            }
+            for (const [element, filter] of filterButtons) {
+                if (
+                    typeof filter !== 'string' &&
+                    typeof activeFilter !== 'string' &&
+                    filter.classification === activeFilter?.classification &&
+                    filter.customfieldvalue === activeFilter?.customfieldvalue
+                ) {
+                    element.classList.add('active');
+                    return;
+                }
+            }
+        });
+
+    const toggleFilterBtn = (
+        <button
+            id={courseFilterDropdownBtnId}
+            class="btn icon-no-margin dropdown-toggle"
+            aria-controls={style.coursesSidebarFilterMenu}
+        >
+            <i class="icon fa fa-filter fa-fw"></i>
+        </button>
+    ) as HTMLButtonElement;
+    const heading = (
+        <>
+            <a class="w-100 text-center" href="/my/courses.php">
+                {LL.features.dashboard.features.layout.myCourses.title()}
+            </a>
+            {toggleFilterBtn}
+        </>
+    ) as DocumentFragment;
+
+    void new Drawer('dashboard-courses')
+        .setSide(Side.Left)
+        .setIcon('graduation-cap')
+        .create()
+        .then(drawer => {
+            // we set the heading here and not before creating as we need to interact with the content
+            drawer.setHeading(heading);
+            // load courses
+            void loadCourseContent(drawer, filterSelection);
+            // if the favourites setting has been changed, reload
+            favouriteCoursesAtTop.onInput(() =>
+                loadCourseContent(drawer, filterSelection)
+            );
+            // and if the active filter has been changed, load the changes too (but only if we're in synced mode)!
+            onActiveFilterChanged(() => {
+                if (courseFilter === '_sync') {
+                    loadCourseContent(drawer, filterSelection);
+                }
+            });
+
+            // toggle the filter selection visibility
+            toggleFilterBtn.addEventListener('click', event => {
+                event.preventDefault();
+                filterSelection.classList.toggle('hidden');
+            });
+            // hide filter selection on window click
+            document.addEventListener('click', event => {
+                if (filterSelection.classList.contains('hidden')) return;
+                const target = event.target;
+                if (!(target instanceof Element)) return;
+                if (
+                    target.closest(`#${filterSelection.id}`) ||
+                    target.closest(`#${toggleFilterBtn.id}`)
+                ) {
+                    return;
+                }
+                filterSelection.classList.add('hidden');
+            });
+            // set filter on click
+            filterSelection.addEventListener('click', event => {
+                const target = event.target;
+                if (!(target instanceof Element)) return;
+                const item =
+                    target.closest<HTMLButtonElement>('.list-group-item');
+                if (!item) return;
+                courseFilter = filterButtons.get(item) ?? '_sync';
+                filterSelection
+                    .querySelector('.active')
+                    ?.classList.remove('active');
+                item.classList.add('active');
+
+                GM_setValue(courseFilterStorageKey, courseFilter);
+
+                filterSelection.classList.add('hidden');
+                loadCourseContent(drawer, filterSelection);
+            });
         });
 };
 
@@ -109,26 +272,11 @@ const loadCourseContent = (drawer: Drawer) => {
  */
 const onload = () => {
     if (!isDashboard || !isLoggedIn) return;
+
     if (coursesSidebarEnabled.value) {
-        void new Drawer('dashboard-courses')
-            .setSide(Side.Left)
-            .setIcon('graduation-cap')
-            .setHeading(
-                (
-                    <>
-                        <a class="w-100 text-center" href="/my/courses.php">
-                            {LL.features.dashboard.features.layout.myCourses.title()}
-                        </a>
-                    </>
-                ) as DocumentFragment
-            )
-            .create()
-            .then(drawer => {
-                void loadCourseContent(drawer);
-                favouriteCoursesAtTop.onInput(() => loadCourseContent(drawer));
-                onActiveFilterChanged(() => loadCourseContent(drawer));
-            });
+        initCourseSidebar();
     }
+
     if (rightSidebarEnabled.value) {
         void new Drawer('dashboard-right')
             .setSide(Side.Right)
