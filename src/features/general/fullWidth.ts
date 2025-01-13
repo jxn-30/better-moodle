2340f5c2db8d96f53594e3502f1d0993feb514a7
jan <jan.boehmerle@ledbrain.de>
2025-01-13 11:47:22 +0100

    fix(fullWidth): fix not waiting for document to be ready

--- src/features/general/fullWidth.ts
+++ src/features/general/fullWidth.ts
@@ -1,16 +1,19 @@
 import './fullWidth.scss';
 import { BooleanSetting } from '@/Settings/BooleanSetting';
 import Feature from '@/Feature';
+import { ready } from '@/DOM';
 
-const enabled = new BooleanSetting('enabled', true)
-    .onInput(() => enabled.feature?.reload())
-    .addAlias('general.fullwidth');
+const enabled = new BooleanSetting('enabled', true).addAlias(
+    'general.fullwidth'
+);
 
 /**
  * Do the DOM operations required to set or unset full width.
  * @param state - Whether to set full width or not.
  */
-const setFullWidth = (state: boolean) => {
+const setFullWidth = async (state: boolean) => {
+    await ready();
+
     document.body.classList.toggle('limitedwidth', !state);
     document
         .getElementById('page-header')
@@ -22,13 +25,15 @@ const setFullWidth = (state: boolean) => {
  * Sets or removes the classes that limit width.
  * @returns void
  */
-const onload = () => setFullWidth(enabled.value);
+const onload = () => void setFullWidth(enabled.value);
+
+enabled.onInput(() => void setFullWidth(enabled.value));
 
 /**
  * Disables fullwidth-mode.
  * @returns void
  */
-const onunload = () => setFullWidth(false);
+const onunload = () => void setFullWidth(false);
 
 export default Feature.register({
     settings: new Set([enabled]),
