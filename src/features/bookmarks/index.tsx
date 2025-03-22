d336259c041e07505536214a4e08d4aeef3554fe
jan <jan.boehmerle@ledbrain.de>
2025-03-22 17:06:02 +0100

    feat(bookmarks): implement bookmark edit modal

--- src/features/bookmarks/index.tsx
+++ src/features/bookmarks/index.tsx
@@ -1,8 +1,8 @@
 import { BooleanSetting } from '@/Settings/BooleanSetting';
 import classnames from 'classnames';
 import FeatureGroup from '@/FeatureGroup';
-import { Modal } from '@/Modal';
 import { LL } from 'i18n';
+import { Modal } from '@/Modal';
 import { render } from '@/templates';
 import { require } from '@/require.js';
 import style from './style.module.scss';
@@ -59,97 +59,151 @@ interface EditRowProps {
     title?: string;
     url?: string;
     controls?: boolean;
+    index?: number;
 }
 interface EditRowElement extends HTMLElement {
-    title: string;
-    url: string;
+    readonly title: string;
+    readonly url: string;
+    index: number;
+    readonly elements: HTMLElement[];
+    remove(): void;
+    before: HTMLElement['before'];
 }
 
 /**
- * @param props
- * @param props.title
- * @param props.url
- * @param props.controls
+ * Creates a component with all inputs and buttons necessary for one Bookmark
+ * @param props - the props for this component
+ * @param props.title - the initial title
+ * @param props.url - the initial url
+ * @param props.controls - wether to show controls to move the bookmark up and down
+ * @param props.index - the initial index
+ * @returns the full component
  */
 const EditRow = ({
     title = '',
     url = '',
     controls = true,
+    index = 0,
 }: EditRowProps): EditRowElement => {
     const titleInput = (
-        <input class="form-control" type="text" placeholder="" />
+        <input
+            class="form-control"
+            type="text"
+            placeholder=""
+            value={title.trim()}
+        />
     ) as HTMLInputElement;
     const urlInput = (
-        <input class="form-control" type="url" placeholder="" />
+        <input
+            class="form-control"
+            type="url"
+            placeholder=""
+            value={url
+                .trim()
+                .replace(/^https:\/\//, '')
+                .trim()}
+        />
     ) as HTMLInputElement;
-    const Row = (
-        <>
-            {titleInput}
-            <div class="input-group">
-                <div class="input-group-prepend">
-                    <span class="input-group-text">https://</span>
-                </div>
-                {urlInput}
+    const urlInputGroup = (
+        <div class="input-group">
+            <div class="input-group-prepend">
+                <span class="input-group-text">https://</span>
             </div>
-            <div class="w-100 d-lg-none"></div>
-            {controls ?
-                <div class="btn-group ml-auto">
-                    <button class="btn btn-outline-seconday" data-action="up">
+            {urlInput}
+        </div>
+    );
+    const controlsGroup =
+        controls ?
+            ((
+                <div class="btn-group ml-auto" data-index={index}>
+                    <button class="btn btn-outline-secondary" data-action="up">
                         <i class="fa fa-arrow-up fa-fw"></i>
                     </button>
-                    <button class="btn btn-outline-seconday" data-action="down">
+                    <button
+                        class="btn btn-outline-secondary"
+                        data-action="down"
+                    >
                         <i class="fa fa-arrow-down fa-fw"></i>
                     </button>
                     <button class="btn btn-outline-danger" data-action="delete">
                         <i class="fa fa-trash fa-fw"></i>
                     </button>
                 </div>
-            :   <></>}
+            ) as HTMLDivElement)
+        :   null;
+    const Row = (
+        <>
+            {titleInput}
+            {urlInputGroup}
+            {controlsGroup}
         </>
     ) as EditRowElement;
 
+    let _index = index;
+
     Object.defineProperty(Row, 'title', {
         /**
-         *
+         * Gets the current title
+         * @returns The title inputs current value, trimmed
          */
         get(): string {
             return titleInput.value.trim();
         },
-        /**
-         * @param newVal
-         */
-        set(newVal: string) {
-            titleInput.value = newVal.trim();
-        },
     });
     Object.defineProperty(Row, 'url', {
         /**
-         *
+         * Gets the current url
+         * @returns The url inputs current value, trimmed and prefixed by https://
          */
         get(): string {
             const input = urlInput.value.trim();
             if (input.startsWith('https://')) return input;
             return `https://${input}`;
         },
+    });
+    Object.defineProperty(Row, 'index', {
         /**
-         * @param newVal
+         * Gets the current index
+         * @returns the current index of this bookmark
          */
-        set(newVal: string) {
-            urlInput.value = newVal
-                .trim()
-                .replace(/^https:\/\//, '')
-                .trim();
+        get(): number {
+            return _index;
+        },
+        /**
+         * Sets the index of this bookmark
+         * @param newVal - the new index
+         */
+        set(newVal: number) {
+            _index = newVal;
+            if (controlsGroup) {
+                controlsGroup.dataset.index = _index.toString();
+            }
         },
     });
-
-    Row.title = title;
-    Row.url = url;
+    Object.defineProperty(Row, 'elements', {
+        value: [titleInput, urlInputGroup, controlsGroup] as const,
+    });
+    Object.defineProperty(Row, 'remove', {
+        /**
+         * Removes all elements of this row
+         * @returns void
+         */
+        value: () => Row.elements.forEach(el => el?.remove()),
+    });
+    Object.defineProperty(Row, 'before', {
+        /**
+         * Adds elements before this row
+         * @param elements - the elements to add
+         * @returns void
+         */
+        value: (...elements: Node[]) => Row.elements.at(0)?.before(...elements),
+    });
 
     return Row;
 };
 
 /**
- *
+ * Creates and opens a modal to add a new bookmark
  */
 const openAddModal = () => {
     const input = (
@@ -180,22 +234,110 @@ const openAddModal = () => {
         })
         .show();
 };
+
 /**
- *
+ * Creates and opens a modal to edit the bookmarks and add new ones
  */
-const openEditModal = () =>
+const openEditModal = () => {
+    const inputs = new Map<number, EditRowElement>();
+    bookmarks.forEach((bookmark, i) =>
+        inputs.set(
+            i,
+            (
+                <EditRow title={bookmark.title} url={bookmark.url} index={i} />
+            ) as EditRowElement
+        )
+    );
+
+    const container = (
+        <div class="fcontainer">
+            <b>{LL.features.bookmarks.modal.title()}</b>
+            <b>{LL.features.bookmarks.modal.url()}</b>
+            <div class="d-none d-sm-block"></div>
+            {...Array.from(inputs.values())}
+        </div>
+    ) as HTMLDivElement;
+    container.addEventListener('click', e => {
+        const target = e.target;
+        if (!(target instanceof Element)) return;
+        const btn = target.closest<HTMLButtonElement>('button[data-action]');
+        const indexStr =
+            btn?.closest<HTMLDivElement>('[data-index]')?.dataset.index;
+        const action = btn?.dataset.action;
+        if (!indexStr || !action) return;
+        const index = Number(indexStr);
+        const row = inputs.get(index);
+        if (!row) return;
+        e.preventDefault();
+        if (action === 'delete') {
+            row.remove();
+            inputs.delete(index);
+            inputs.values().forEach((row, i) => (row.index = i));
+        } else if (action === 'up') {
+            const prevRow = inputs.get(index - 1);
+            if (prevRow) {
+                [prevRow.index, row.index] = [row.index, prevRow.index];
+                prevRow.before(...row.elements);
+                inputs.set(index, prevRow);
+                inputs.set(index - 1, row);
+            }
+        } else if (action === 'down') {
+            const nextRow = inputs.get(index + 1);
+            if (nextRow) {
+                [nextRow.index, row.index] = [row.index, nextRow.index];
+                row.before(...nextRow.elements);
+                inputs.set(index, nextRow);
+                inputs.set(index + 1, row);
+            }
+        }
+    });
+
+    const addBtn = (
+        <button class="btn btn-outline-success pull-right">
+            <i class="fa fa-plus fa-fw"></i>
+        </button>
+    ) as HTMLButtonElement;
+    addBtn.addEventListener('click', e => {
+        e.preventDefault();
+        const newInput = (
+            <EditRow
+                title={document.title}
+                url={window.location.href}
+                index={inputs.size}
+            />
+        ) as EditRowElement;
+        inputs.set(inputs.size, newInput);
+        container.append(newInput);
+    });
+
     new Modal({
         type: 'SAVE_CANCEL',
         title: LL.features.bookmarks.edit(),
-        body: <></>,
+        body: (
+            <form class={classnames('mform', style.form)}>
+                {container}
+                {addBtn}
+            </form>
+        ),
         large: true,
         removeOnClose: true,
     })
-        .onSave(() => console.log('closi closi :)'))
+        .onSave(() => {
+            bookmarks.splice(
+                0,
+                bookmarks.length,
+                ...Array.from(inputs.values())
+                    .toSorted((a, b) => a.index - b.index)
+                    .map(({ title, url }) => ({ title, url }))
+            );
+            saveBookmarks();
+        })
         .show();
+};
 
 /**
- *
+ * Renders the dropdown with the current bookmarks and puts it into the correct position in DOM.
+ * @returns a Promise that resolves when the rendering finished
  */
 const renderDropdown = () =>
     render('core/custom_menu_item', {
@@ -275,23 +417,24 @@ const renderDropdown = () =>
         });
 
 /**
- *
+ * Triggers rendering or removal of the dropdown, based on the settings state
  */
 const onload = () => {
     if (enabled.value) {
         void renderDropdown();
     } else {
-        navbarItem?.remove();
+        onunload();
     }
 };
 
 enabled.onInput(() => void onload());
 
 /**
- *
+ * Removes the dropdown from DOM and deletes the instance.
  */
 const onunload = () => {
     navbarItem?.remove();
+    navbarItem = null;
 };
 
 export default FeatureGroup.register({
