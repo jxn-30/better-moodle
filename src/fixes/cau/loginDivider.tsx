a69b2f3238cd00f5825809f203896a75b5551ffa
Yorik Hansen <github@yorik.dev>
2025-05-31 00:45:31 +0200

    regex.exec -> regex.test

--- src/fixes/cau/loginDivider.tsx
+++ src/fixes/cau/loginDivider.tsx
@@ -3,7 +3,7 @@ import { ready } from '@/DOM';
 if (window.location.pathname.startsWith('/login/')) {
     await ready();
     document.querySelectorAll<HTMLParagraphElement>('p').forEach(p => {
-        if (/_{5,}/u.exec(p.innerText)) {
+        if (/_{5,}/u.test(p.innerText)) {
             p.replaceWith(<div className="login-divider" />);
         }
     });
