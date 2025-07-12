3729629aa474a3e2fe5b7991381735abe333ee00
jan <jan.boehmerle@ledbrain.de>
2025-07-12 11:42:39 +0200

    test: implement a test:src workflow

--- vitest.userscript.config.ts
+++ vitest.userscript.config.ts
@@ -2,17 +2,23 @@ import { join } from 'node:path';
 import { defineConfig, mergeConfig } from 'vitest/config';
 import viteConfig, { fileName } from './vite.config';
 
-// console.log(join(__dirname, 'dist', fileName));
-// viteConfig.define.__USERSCRIPT_FILE__ = join(__dirname, 'dist', fileName);
-
 export default mergeConfig(
     viteConfig,
     defineConfig({
         test: {
+            reporters: ['default', 'json'],
             include: [
                 '__tests__/__userscript__/**/*.test.tsx',
                 '__tests__/__userscript__/**/*.test.ts',
             ],
+            coverage: {
+                thresholds: {
+                    lines: 80,
+                    branches: 80,
+                    functions: 80,
+                    statements: 80,
+                },
+            },
             environment: 'node',
             setupFiles: ['__tests__/matchers.ts'],
             testTimeout: 300_000_000, // 5 million minutes
