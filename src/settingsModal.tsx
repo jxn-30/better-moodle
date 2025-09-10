1cf9ad46fa0f91bb893c78043c1f1b7a6a6f954a
jan <jan.boehmerle@ledbrain.de>
2025-09-10 23:00:57 +0200

    fix(settings): fix search icon not always working

--- src/settingsModal.tsx
+++ src/settingsModal.tsx
@@ -347,7 +347,7 @@ const SearchStyle = <style></style>;
 const SearchWrapper = (
     <div className="form-group ml-auto mr-auto">
         {SearchInput}
-        <i class="fa-solid fa-magnifying-glass"></i>
+        <i class="fa-solid fa-search"></i>
         {SearchStyle}
     </div>
 );
