785275ea74c76db4f943a47239a5475e8cba4308
jan <jan.boehmerle@ledbrain.de>
2025-09-22 20:12:06 +0200

    refactor(mustache): remove more unnecessary whitespaces

--- vite.config.ts
+++ vite.config.ts
@@ -579,6 +579,9 @@ export default defineConfig({
                     .replace(/\{\{!.*?\}\}/gs, '') // remove mustache comments
                     .replace(/\\n/g, '') // remove linebreaks
                     .replace(/(?<=\{\{[<>/$#^]?)\s+|\s+(?=\}\})/g, '') // remove unnecessary whitespaces in mustache statements
+                    .replace(/(?<=<[a-z]+)\s+/, ' ') // remove unnecessary whitespace in html tags (after tag name)
+                    .replace(/(?<=")\s+(?=>)/, '') // remove unnecessary whitespace in html tags (end of tag)
+                    .replace(/\s+(?=\/>)/, '') // remove unnecessary whitespace in self-closing html tags
                     .replace(/ {3,}/g, '  '); // reduce white spaces to a maximum of 2. This may break at <pre> tags but that isn't an issue yet.
             },
         },
