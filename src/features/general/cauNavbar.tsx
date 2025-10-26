56dec96bdf6fd61d4dfd93ebed2ba2b1bb5cd696
Yorik Hansen <github@yorik.dev>
2025-10-26 16:34:42 +0100

    fix(cauNavbar): make buttons translateable by removing the weird icon hacks

--- src/features/general/cauNavbar.tsx
+++ src/features/general/cauNavbar.tsx
@@ -1,14 +1,35 @@
 import Feature from '@/Feature';
 import globalStyle from '!/index.module.scss';
+import { LLF } from 'i18n';
 import { ready } from '@/DOM';
 // import { SelectSetting } from '@/Settings/SelectSetting';
 import styles from './cauNavbar.module.scss';
+
+const LL = LLF('general', 'cauNavbar');
 
 // const mode = new SelectSetting('mode', 'default', [
 // 	'default',
 // ]);
 
 const localNavbarPlusDiv = <div id={styles.localnavbarplus} />;
+
+const localnavbarplusSos = (
+    <div>
+        <i class="fa fa-fw icon fa-bell"></i>
+        <span>{LL.buttons.sos.sos()}</span>
+    </div>
+);
+const localnavbarplusDiscrimination = (
+    <div>
+        <span>{LL.buttons.discrimination.discriminationSexism()}</span>
+        <span>
+            <i class="fa fa-fw icon fa-arrow-right"></i>{' '}
+            {LL.buttons.discrimination.report()}{' '}
+            <i class="fa fa-fw icon fa-arrow-right"></i>{' '}
+            {LL.buttons.discrimination.getHelp()}
+        </span>
+    </div>
+);
 
 /**
  * Handles the "localnavbarplus" Buttons.
@@ -24,6 +45,12 @@ const reload = async () => {
             localNavbarPlusDiv.append(el);
         }
     );
+    localNavbarPlusDiv
+        .querySelector('#localnavbarplus-sos a i')
+        ?.replaceWith(localnavbarplusSos);
+    localNavbarPlusDiv
+        .querySelector('#localnavbarplus-discrimination a i')
+        ?.replaceWith(localnavbarplusDiscrimination);
     document.getElementById('usernavigation')?.before(localNavbarPlusDiv);
 
     const page = document.getElementById('page');
