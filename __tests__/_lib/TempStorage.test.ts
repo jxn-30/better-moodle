8752a14e435204f9558d7e891c7412b837fa2e8d
jan <jan.boehmerle@ledbrain.de>
2025-11-30 18:26:30 +0100

    test: add tests for TempStorage lib

---
+++ __tests__/_lib/TempStorage.test.ts
@@ -0,0 +1,13 @@
+import tempStorage from '@/TempStorage';
+import { describe, expect, it } from 'vitest';
+
+describe('TempStorage', () => {
+    it('initializes settingsRequireReload to false', () => {
+        expect(tempStorage.settingsRequireReload).toBe(false);
+    });
+
+    it('settingsRequireReload can be changed', () => {
+        tempStorage.settingsRequireReload = true;
+        expect(tempStorage.settingsRequireReload).toBe(true);
+    });
+});
