65c1896915d192a1f82b73dbbd299ecf84b81a56
jan <jan.boehmerle@ledbrain.de>
2025-12-05 21:16:03 +0100

    test: add tests for @/times lib

---
+++ __tests__/_lib/times.test.ts
@@ -0,0 +1,22 @@
+import * as times from '@/times';
+import { describe, expect, it } from 'vitest';
+
+describe('times', () => {
+    it('the time durations should be correct', () => {
+        expect(times).toMatchObject({
+            ONE_SECOND: 1000,
+
+            TEN_SECONDS: 10_000,
+            THIRTY_SECONDS: 30_000,
+
+            ONE_MINUTE: 60_000,
+            FOUR_MINUTES: 240_000,
+            FIVE_MINUTES: 300_000,
+            TEN_MINUTES: 600_000,
+
+            ONE_HOUR: 3_600_000,
+
+            ONE_DAY: 86_400_000,
+        });
+    });
+});
