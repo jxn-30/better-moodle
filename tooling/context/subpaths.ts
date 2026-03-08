b50ea281033766a93c206247d195600ec336282c
jan <jan.boehmerle@ledbrain.de>
2026-03-08 16:29:35 +0100

    build: fix globs on windows #1025

--- tooling/context/subpaths.ts
+++ tooling/context/subpaths.ts
@@ -9,6 +9,15 @@ import path from 'node:path';
 export const resolve = (path: string) =>
     fileURLToPath(import.meta.resolve(path)); // import.meta.resolve returns a file:// url
 
+/**
+ * Converts a path into its corresponding POSIX form.
+ * This is needed e.g. for globs
+ * @param pathStr - a path in either Windows style, POSIX style or mixed.
+ * @returns a valid POSIX path
+ */
+export const toPOSIX = (pathStr: string) =>
+    pathStr.replaceAll(path.sep, path.posix.sep);
+
 export const root = path.dirname(resolve('#_/package.json'));
 
 export const configsBase = path.dirname(resolve('#_configs/_config.d.ts'));
